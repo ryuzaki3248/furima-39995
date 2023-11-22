82dfa9ea47d37af574df0b55bd9e1590da2cb6a9
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-22 16:46:50 +0900

    新規登録できるように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
-class ApplicationController < ActionController::Base
-	before_action :basic_auth
 
+
+class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +10,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :full_width_last_name, :full_width_first_name, :full_width_last_name_kana, :full_width_first_name_kana, :birth_date])
   end
 end
