ad45c2dfa206e68092bebcdc925a9fade5513cd6
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-22 13:06:23 +0900

    モデルにてバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :nickname, :email, :encrypted_password, :full_width_last_name, 
+          :full_width_first_name, :full_width_last_name_kana, 
+          :full_width_first_name_kana, :birth_date, presence: true
+
+validates :email, uniqueness: true
+
 end
