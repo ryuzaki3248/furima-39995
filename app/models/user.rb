d835ce76e28e27f279cb87525af225660267bd05
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-24 17:22:06 +0900

    バリデーションを追記＆変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :nickname,  :full_width_last_name, 
-          :full_width_first_name, :full_width_last_name_kana, 
-          :full_width_first_name_kana, :birth_date, presence: true
 
-
-
+  # デバイスのバリデーション
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must be a combination of letters and numbers" }
+  validates :full_width_last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
+  validates :full_width_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
+  validates :full_width_last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
+  validates :full_width_first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
+  validates :birth_date, presence: true
 end
