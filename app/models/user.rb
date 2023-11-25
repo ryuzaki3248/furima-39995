17cd2bceb42409736bc81d1914fa1d9b79d7d43b
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-25 15:53:59 +0900

    コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
 
   # デバイスのバリデーション
   validates :nickname, presence: true
-  # validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must be a combination of letters and numbers" }
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
   validates :full_width_last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
   validates :full_width_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
