3c416d09134a7367a602c7c72df9093c99d59a9c
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-25 21:14:28 +0900

    アクティブストレージ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,33 @@ class ItemsController < ApplicationController
     # @items = Item.all
 	end
 
+ def new
+  @item = Item.new
+ end
+
+
+
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+
+private
+
+def item_params
+  binding.pry
+  params.require(:item).permit(:image, :product_name, :category_id, :product_condition_id, :burden_of_shipping_charges_id, :prefecture_id, :number_of_days_until_shipping_id, :selling_price).merge(user_id: current_user.id)
+end
+
+
+
+
+
+
+
 end
