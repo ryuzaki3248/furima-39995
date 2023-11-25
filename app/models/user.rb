84f538938929b6e342126fef6bcb40c0e08a6409
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-25 16:16:38 +0900

    /\A[ァ-ヶー]+\z/に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
   validates :full_width_last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
   validates :full_width_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "must be full-width characters in hiragana, katakana, or kanji" }
-  validates :full_width_last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
-  validates :full_width_first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "must be full-width katakana characters" }
+  validates :full_width_last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "must be full-width katakana characters" }
+  validates :full_width_first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "must be full-width katakana characters" }
   validates :birth_date, presence: true
 end
