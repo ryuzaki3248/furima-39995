93b2775e35b922af46ebf0675cb0544d0adbb0d1
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-01 11:48:38 +0900

    商品情報を変更、更新できるように記述しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  # before_action :authenticate_user!, except: [:index, :show ]
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,10 +24,32 @@ def create
   end
 end
 
+def edit
+  @item = Item.find(params[:id])
+  if @item.user_id != current_user.id
+    redirect_to root_path
+  end
+end
+
+
+def update
+  # @item = Item.edit(item_params)
+  @item = Item.find(params[:id])  
+  # if @item.update(item_params)
+  #   redirect_to root_path
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+
 private
 
 def item_params
   params.require(:item).permit(:image, :product_name, :product_description, :category_id, :product_condition_id, :burden_of_shipping_charges_id, :prefecture_id, :number_of_days_until_shipping_id, :selling_price).merge(user_id: current_user.id)
 end
+
 
 end
