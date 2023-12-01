2ca8daf33b6c79845079776114d3d3273e1e9aa5
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-01 16:53:57 +0900

    before_action :set_item, only: にdestroy追加、コントローラー側にもログイン時のみ自身が出品した商品のみ削除できるように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -37,8 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if current_user == @item.user
+      @item.destroy 
+    end
     redirect_to root_path
   end
 
