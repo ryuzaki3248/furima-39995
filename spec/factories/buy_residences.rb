6182292b86c84e01b8c197dc11b6b978d8407acc
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-06 12:55:57 +0900

    テストコード実装

---
+++ spec/factories/buy_residences.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :buy_residence do
+    post_code {"123-4567"}
+    prefecture_id {"2"}
+    municipalities {"横浜市"}
+    street_address {"青山1−1"}
+    building_name {"サーパス101"}    
+    telephone_number {"09012341234"}    
+    token {"tok_abc123456789123456789"}
+    
+  end
+end
