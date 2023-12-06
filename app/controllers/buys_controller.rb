8116dd15d0dd94e946d6073c9ac18cea2425e736
ryuzaki3248 <gekidannax7@gmail.com>
2023-12-06 16:56:17 +0900

    見やすい記述に変更

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,25 +1,19 @@
 class BuysController < ApplicationController
-	before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
-   def index
-    if user_signed_in?
-      @item = Item.find(params[:item_id])
-      
-      if @item.sold_out?
-        redirect_to root_path
-      else
-        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-        @buy_residence = BuyResidence.new
-      end
+  def index
+    if @item.sold_out?
+      redirect_to root_path
     else
-      redirect_to new_user_session_path
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      @buy_residence = BuyResidence.new
     end
   end
 
   def create
-    # binding.pry		
     @buy_residence = BuyResidence.new(buy_residence_params)
-    @item =  Item.find(params[:item_id])
+
     if @buy_residence.valid?
       pay_item
       @buy_residence.save
@@ -31,26 +25,27 @@ class BuysController < ApplicationController
 
   private
 
-	def buy_residence_params
-		params.require(:buy_residence).permit(
-      :post_code,
-       :prefecture_id, 
-       :municipalities, 
-       :street_address, 
-       :building_name, 
-       :telephone_number
-      ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-	end
-
-def pay_item
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  Payjp::Charge.create(
-    amount: @item.selling_price,  # 商品の値段
-    card: buy_residence_params[:token],    # カードトークン
-    currency: 'jpy'                 # 通貨の種類（日本円）
-  )
-  
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
-end
+  def buy_residence_params
+    params.require(:buy_residence).permit(
+      :post_code,
+      :prefecture_id,
+      :municipalities,
+      :street_address,
+      :building_name,
+      :telephone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.selling_price,  # 商品の値段
+      card: buy_residence_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+end
